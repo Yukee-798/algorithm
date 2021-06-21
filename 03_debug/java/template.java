5e919ed3e6a1b0dd9f27fabbf54b2b6a98287db4
Yukee-798 <909397116@qq.com>
2021-06-22 02:47:21 +0800

    Update Java template.

--- 03_debug/java/template.java
+++ 03_debug/java/template.java
@@ -4,24 +4,8 @@ import java.math.*;
 public class Main {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
-
     String[] sp;
 
-    // String => int
-    int i(String s) {
-        return Integer.parseInt(s);
-    }
-
-    // int => String
-    String s(int i) {
-        return String.valueOf(i);
-    }
-
-    // read
-    void r() throws Exception{
-        sp = reader.readLine().split(" ");
-    }
-    
 
     void run() throws Exception {
         
@@ -29,7 +13,10 @@ public class Main {
         log.flush();
     }
 
-
+    
+    int i(String s) {return Integer.parseInt(s);}// String => int
+    String s(int i) {return String.valueOf(i);}// int => String
+    void r() throws Exception{ sp = reader.readLine().split(" ");} // read
     public static void main(String[] args) throws Exception {new Main().run();}
 }
 
